8affda2a03f9e89cd9f78674d00c66c562635647
jimikajon <jimi.kajon@gmail.com>
2020-02-13 13:28:32 -0500

    removed kittens

--- huskies.js
+++ huskies.js
@@ -6,26 +6,22 @@ const huskies = [
         "imageAlt": "white husky"
     },
 
-
-
-
-
-    {
-        "url": "http://www.flickr.com/photos/seanmolin/7028040701/",
-        "title": "Little Low-Light Monsters (D800 @ ISO 25,600) by Sean Molin Photography, on Flickr",
-        "imageUrl": "http://farm8.staticflickr.com/7239/7028040701_6de179d6fb_b.jpg",
-        "imageAlt": "Little Low-Light Monsters (D800 @ ISO 25,600)"
-    },
-    {
-        "url": "http://www.flickr.com/photos/merlijnhoek/3184818586/",
-        "title": "Muffin // Our new kitten by Merlijn Hoek, on Flickr",
-        "imageUrl": "http://farm4.staticflickr.com/3331/3184818586_4ff359c537_b.jpg",
-        "imageAlt": "Muffin // Our new kitten"
-    },
-    {
-        "url": "http://www.flickr.com/photos/merlijnhoek/2841785343/",
-        "title": "Crazy Kitten? // Nikkor 50mm 1.4 by Merlijn Hoek, on Flickr",
-        "imageUrl": "http://farm4.staticflickr.com/3033/2841785343_e166fb4396_b.jpg",
-        "imageAlt": "Crazy Kitten? // Nikkor 50mm 1.4"
-    }
+    // {
+    //     "url": "http://www.flickr.com/photos/seanmolin/7028040701/",
+    //     "title": "Little Low-Light Monsters (D800 @ ISO 25,600) by Sean Molin Photography, on Flickr",
+    //     "imageUrl": "http://farm8.staticflickr.com/7239/7028040701_6de179d6fb_b.jpg",
+    //     "imageAlt": "Little Low-Light Monsters (D800 @ ISO 25,600)"
+    // },
+    // {
+    //     "url": "http://www.flickr.com/photos/merlijnhoek/3184818586/",
+    //     "title": "Muffin // Our new kitten by Merlijn Hoek, on Flickr",
+    //     "imageUrl": "http://farm4.staticflickr.com/3331/3184818586_4ff359c537_b.jpg",
+    //     "imageAlt": "Muffin // Our new kitten"
+    // },
+    // {
+    //     "url": "http://www.flickr.com/photos/merlijnhoek/2841785343/",
+    //     "title": "Crazy Kitten? // Nikkor 50mm 1.4 by Merlijn Hoek, on Flickr",
+    //     "imageUrl": "http://farm4.staticflickr.com/3033/2841785343_e166fb4396_b.jpg",
+    //     "imageAlt": "Crazy Kitten? // Nikkor 50mm 1.4"
+    // }
 ];
