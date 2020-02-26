e2bc26c139cb654273ac28e8b989cf05d90b92b8
WenWithLemon <wenwu170817@gmail.com>
2020-02-25 23:33:46 -0500

    added several huskies.

--- huskies.js
+++ huskies.js
@@ -35,5 +35,73 @@ const huskies = [
         "imageUrl": "//live.staticflickr.com/5095/5419396155_4284bd9ca6.jpg",
         "imageAlt": "Aura & Talon",
     },
+    {
+        "url": "https://www.flickr.com/photos/collins_family/28562398951/in/photolist-KvXL7k-qDfgFz-rxvgNB-28DWr4H-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f",
+        "title": "sleepy z by Christian Collins, on Flickr",
+        "imageUrl": "//live.staticflickr.com/8040/28562398951_4de05be9c4.jpg",
+        "imageAlt": "sleepy z",
+    },
+    {
+        "url": "https://www.flickr.com/photos/tambako/4525212851/in/photolist-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f-5jwzXQ-sGQUJ-5jwzKS-i8rM3",
+        "title": "Cute, funny and yawning by Tambako the Jaguar, on Flickr",
+        "imageUrl": "//live.staticflickr.com/4054/4525212851_3b6912b15b.jpg",
+        "imageAlt": "Cute, funny and yawning ,
+    },
+    {
+        "url": "https://www.flickr.com/photos/col_and_tasha/15925980897/in/photolist-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f-5jwzXQ-sGQUJ-5jwzKS-i8rM3/",
+        "title": "Disgwylfa - September by Disgwylfa, on Flickr",
+        "imageUrl": "//live.staticflickr.com/7552/15925980897_cd5be06e99.jpg",
+        "imageAlt": "Disgwylfa - September ",
+    },
+    {
+        "url": "https://www.flickr.com/photos/collins_family/9208520285/in/photolist-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f-5jwzXQ-sGQUJ-5jwzKS-i8rM3/",
+        "title": "happy zima, happy sasha by Christian Collins, on Flickr",
+        "imageUrl": "//live.staticflickr.com/7346/9208520285_a7088295c1.jpg",
+        "imageAlt": "happy zima, happy sasha",
+    },
+    {
+        "url": "https://www.flickr.com/photos/32713462@N08/8748526530/in/photolist-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f-5jwzXQ-sGQUJ-5jwzKS-i8rM3/",
+        "title": "Jailed by mare.bowe, on Flickr",
+        "imageUrl": "//live.staticflickr.com/7296/8748526530_5e6c889b26.jpg",
+        "imageAlt": "Jailed",
+    },
+    {
+        "url": "https://www.flickr.com/photos/emyanmei/6880882319/in/photolist-7TSU8e-7QNa1L-iPxu2e-7QvFkP-qZ4KbC-5zmrzs-2aw4T72-8omusR-qgjPgx-qbSKBo-didy3-ydZmnB-81Hf3j-9jmZzk-6nYWsf-aDJta7-9DLMSA-aDJsjC-xWsUtH-25dA7x3-7Mo2MX-fuYpPh-f2J5g8-ek5u9j-asPyJ5-ayc2N1-bp47Q8-bu3jA4-bp47Qz-fwXYeU-5zhauc-dDjija-4EtT3G-4JuwXn-7JbqGC-bp47QM-32z4Zf-qVCerE-9n7TJK-dc4tGt-amJ7ck-84JmtL-ayc2Qj-qDhdfx-z3938-S662f-5jwzXQ-sGQUJ-5jwzKS-i8rM3/",
+        "title": "Funny face...and pose by Emyan, on Flickr",
+        "imageUrl": "//live.staticflickr.com/7054/6880882319_215583bb1a.jpg",
+        "imageAlt": "Funny face...and pose",
+    },
+    {
+        "url": "https://www.flickr.com/photos/43089317@N04/8065132121/in/photolist-dhFUN6-e6rvmc-6wgTSc-bYWUpA-b9Gt6F-dD9dHS-7YBBGu-RVhxaN-kwkjQ8-dCTrBr-RBJCdB-e5rSAM-39xw8-hr1ciX-e5MZJ3-64d9Mf-6Kk8qx-aJqn7R-a2uAW3-esPh6E-smyRxV-bngoFN-db9kyL-e5GCqa-b9GC7D-LkdKb-48a3Cq-2TrAT9-9U9SiH-7A67v8-bAbfZn-22RbFZ-vJ7Po-35bePd-hr1quX-ArikDn-edqXq-e5Erj6-e5HA2K-6iw1RC-e5PdsW-4Xw5G-j6BHsi-SivHjy-4fk41Q-6mbRgJ-e5L5so-5pgsxx-9wMj1L-esPk5G",
+        "title": "huskies by sdkscorp, on Flickr",
+        "imageUrl": "//live.staticflickr.com/8169/8065132121_3c823391d9.jpg",
+        "imageAlt": "huskies",
+    },
+    {
+        "url": "https://www.flickr.com/photos/denverjeffrey/8836091296/in/photolist-dhFUN6-e6rvmc-6wgTSc-bYWUpA-b9Gt6F-dD9dHS-7YBBGu-RVhxaN-kwkjQ8-dCTrBr-RBJCdB-e5rSAM-39xw8-hr1ciX-e5MZJ3-64d9Mf-6Kk8qx-aJqn7R-a2uAW3-esPh6E-smyRxV-bngoFN-db9kyL-e5GCqa-b9GC7D-LkdKb-48a3Cq-2TrAT9-9U9SiH-7A67v8-bAbfZn-22RbFZ-vJ7Po-35bePd-hr1quX-ArikDn-edqXq-e5Erj6-e5HA2K-6iw1RC-e5PdsW-4Xw5G-j6BHsi-SivHjy-4fk41Q-6mbRgJ-e5L5so-5pgsxx-9wMj1L-esPk5G/",
+        "title": "Siberian Husky Puppies 2013-05-25 by Jeffrey Beall, on Flickr",
+        "imageUrl": "//live.staticflickr.com/5349/8836091296_07b96643a9.jpg",
+        "imageAlt": "Siberian Husky Puppies 2013-05-25 ",
+    },
+    {
+        "url": "https://www.flickr.com/photos/mprinke/17298016159/in/photolist-dhFUN6-e6rvmc-6wgTSc-bYWUpA-b9Gt6F-dD9dHS-7YBBGu-RVhxaN-kwkjQ8-dCTrBr-RBJCdB-e5rSAM-39xw8-hr1ciX-e5MZJ3-64d9Mf-6Kk8qx-aJqn7R-a2uAW3-esPh6E-smyRxV-bngoFN-db9kyL-e5GCqa-b9GC7D-LkdKb-48a3Cq-2TrAT9-9U9SiH-7A67v8-bAbfZn-22RbFZ-vJ7Po-35bePd-hr1quX-ArikDn-edqXq-e5Erj6-e5HA2K-6iw1RC-e5PdsW-4Xw5G-j6BHsi-SivHjy-4fk41Q-6mbRgJ-e5L5so-5pgsxx-9wMj1L-esPk5G/",
+        "title": "Nachmittagsschlaf by m.prinke, on Flickr",
+        "imageUrl": "//live.staticflickr.com/7679/17298016159_8dc8a61bec.jpg",
+        "imageAlt": "Nachmittagsschlaf ",
+    },
+    {
+        "url": "https://www.flickr.com/photos/99743387@N06/14281675743/in/photolist-nL2jCF-8T3umw-bCqHrB-7jy6AZ-bpvMe9-aLF5bB-7JjvtP-pKDRet-6ER64n-6mbRgJ-5hUc8P-aHc34F-pjhxV-nJzJY-e5L5so-6aR3Hk-5rE8AG-QZJAw-e6x9i1-jjvSy-7YyojR-oMDLG5-hqZbwm-e5rJX6-bpvMjA-d5Yog3-tqgyRp-5pgsxx-4Kix8a-82ZCUY-pXNTJK-qpdBA-4MfXou-4EASko-4vsbrk-icSxW-nqm7NR-4pkEr2-79wqsd-e6x9Fj-9wMj1L-4EALGJ-esPk5G-8wF3DB-5JPc6d-a2sCL7-JSXZTa-e5JcFo-p2qx-4foRsU",
+        "title": "Untitled by TaniaShi, on Flickr",
+        "imageUrl": "//live.staticflickr.com/5512/14281675743_cf1ea30278.jpg",
+        "imageAlt": "Untitled ",
+    },
+    {
+        "url": "https://www.flickr.com/photos/118490865@N07/29981063712/in/photolist-MFjMHA-eemZ5a-2Gaar7-9wJkdc-u9UB6o-e5EsRa-5VmnWW-6irSzM-7Fjktq-9fWHxA-91FQ1A-52WDGE-9fWQcE-4ckfJV-5eqzgJ-4yJbC9-4Euy13-cq2HNd-umGV9J-5rZ38b-5rUUU2-5rUA5P-8YjkV3-492Bni-7uca2n-6y4tpe-84nnFk-5rYWed-HuqJLs-4muzVt-7sy4UU-e5GDcM-cCyZN1-a2sDe3-4ZgXWZ-34ZXWb-64FTyd-MSzqkn-rroaxV-4CTkJE-hqoCJQ-4xeNuW-9fWLQh-5emdrF-8YjkB3-58FndY-bs9wES-5GZtnY-qvewT-5em8yz",
+        "title": "Siperianhusky by tiiaaj, on Flickr",
+        "imageUrl": "//live.staticflickr.com/8378/29981063712_64a0e56945.jpg",
+        "imageAlt": "Siperianhusky ",
+    },
+
+
 
 ];
